78d336f76156d222d3c835e7544664d34cdf1096
Juan C. Francisco <tzav.code@gmail.com>
2024-09-11 19:57:55 -0300

    ADD Tarea Operadores de Asignación y Comparación

---
+++ tareaOpAsignacionComparacion.js
@@ -0,0 +1,27 @@
+const numero1 = 15
+const numero2 = 20
+const numero3 = '25'
+
+// 1. Muestre por consola si el numero1 es mayor o igual que numero2.
+
+console.log("¿Es numero1 mayor o igual que numero2 ?:", numero1 >= numero2)
+
+// 2. Muestre por consola si el numero1 es menor o igual que numero2.
+
+console.log("¿Es numero1 menor o igual que numero2 ?:", numero1 <= numero2)
+
+// 3. Muestre por consola si el numero1 es menor que numero3.
+
+console.log("¿Es numero1 menor que numero2 ?:", numero1 < numero3)
+
+// 4. Muestre por consola si el numero3 es menor que numero2.
+
+console.log("¿Es numero3 menor que numero2 ?:", numero3 < numero2)
+
+// 5. Muestre por consola si el numero3 es estrictamente diferente que numero1.
+
+console.log("¿Es numero3 estrictamente diferente de numero1 ?:", numero3 !== numero1)
+
+// 6. Muestre por consola si el numero1 es estrictamente igual al numero2.
+
+console.log("¿Es numero3 estrictamente diferente de numero1 ?:", numero1 === numero2)

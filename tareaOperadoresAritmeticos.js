1b7fba8969812f8c34e52f565e39919582858bee
Juan C. Francisco <tzav.code@gmail.com>
2024-09-11 19:41:18 -0300

    ADD Tarea Operadores Aritmeticos

---
+++ tareaOperadoresAritmeticos.js
@@ -0,0 +1,8 @@
+const numero1 = 20
+const numero2 = 40
+
+console.log("Suma:", numero1 + numero2)
+console.log("Resta:", numero1 - numero2)
+console.log("Multiplicación:", numero1 * numero2)
+console.log("División:", numero1 / numero2)
+console.log("Modulo", numero2 % numero1)

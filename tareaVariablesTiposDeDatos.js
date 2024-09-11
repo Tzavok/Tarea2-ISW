2df9eafbe611efe9aac33ea88496742d121360bb
Juan C. Francisco <tzav.code@gmail.com>
2024-09-11 19:34:34 -0300

    UPDATE README.md y ADD Tarea Variables y Tipos de Datos

---
+++ tareaVariablesTiposDeDatos.js
@@ -0,0 +1,12 @@
+const nombre = "Juan Carlos"
+console.log("Nombre: ", nombre)
+console.log("Tipo variable 'nombre':",typeof nombre)
+
+const edad = 30
+console.log("Edad:", edad)
+console.log("Tipo variable 'edad':",typeof edad)
+
+const puedoConducir = true
+console.log("Puede Conducir:", puedoConducir)
+console.log("Tipo variable 'edad':",typeof puedoConducir)
+
